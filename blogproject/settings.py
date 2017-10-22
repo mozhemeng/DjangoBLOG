2cf45495ce0f2be689afeea7ba56786d81a66a17
mozhemeng <371067260@qq.com>
2017-10-22 22:06:48 +0800

    fix setting

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '120.24.184.76']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ']
 
 
 # Application definition
